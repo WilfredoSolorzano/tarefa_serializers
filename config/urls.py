286122781bcb75034b3dfb9f80e02a9431fb8177
WilfredoSolorzano <wsolorzano00@gmail.com>
2025-10-17 19:03:47 -0300

    Adiciona a funcionalidade paginaçao

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -26,6 +26,7 @@ router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
 
 urlpatterns = [
+    path("__debug__/", include(debug_toolbar.urls)),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
